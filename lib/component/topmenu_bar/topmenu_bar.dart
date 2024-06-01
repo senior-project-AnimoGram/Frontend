e270e2175f7765288e08a9399b80fd5a91c736d5
Yoon Sang Jo <jsy440720@gmail.com>
2024-06-01 20:28:51 +0900

    refactor: 코드 리팩토링

--- lib/component/topmenu_bar/topmenu_bar.dart
+++ lib/component/topmenu_bar/topmenu_bar.dart
@@ -24,7 +24,7 @@ class TopMenuBar extends StatelessWidget {
                   onPressed: () {
                     MainScreenController mainScreenController = Get.find<MainScreenController>();
                     mainScreenController.pageIndex.value = 0;
-                    mainScreenController.appBarText.value = "주변 동물병원 안내";
+                    mainScreenController.appBarText.value = "지도";
 
                     mainScreenController.hospitalState.value = true;
                     mainScreenController.adoptionState.value = false;
